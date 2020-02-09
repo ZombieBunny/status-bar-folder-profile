3adff1d122e8cbf016756940e540d038294cb25e
Lee <leevashan.reddi@aax.co.za>
2020-02-09 16:52:15 +0200

    removed commented out code

--- src/extension.ts
+++ src/extension.ts
@@ -13,18 +13,6 @@ function transformFolderName(folderName: string): string {
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  // let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-  // 	// The code you place here will be executed every time your command is executed
-
-  // 	// Display a message box to the user
-  // 	vscode.window.showInformationMessage('Hello the World!');
-  // });
-
   const statusBarItem = vscode.window.createStatusBarItem(
     vscode.StatusBarAlignment.Left,
     100000000
@@ -32,7 +20,6 @@ export function activate(context: vscode.ExtensionContext) {
 
   function runStatusBarFunction() {
     // TODO: fetch command from settings
-
     if (vscode.workspace.name) {
       statusBarItem.text = transformFolderName(vscode.workspace.name);
       statusBarItem.show();
