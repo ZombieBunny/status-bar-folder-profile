3f609e768b1c39bb48f8621ac6f301491126e27b
Lee <leevashan.reddi@aax.co.za>
2020-02-09 16:50:36 +0200

    updated position of label on status bar

--- src/extension.ts
+++ src/extension.ts
@@ -2,13 +2,19 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 
+function transformFolderName(folderName: string): string {
+  return folderName
+    .toLowerCase()
+    .split("-")
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join("-");
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "foldername" is now active!');
-
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
@@ -20,23 +26,25 @@ export function activate(context: vscode.ExtensionContext) {
   // });
 
   const statusBarItem = vscode.window.createStatusBarItem(
-    vscode.StatusBarAlignment.Right,
-    1000
+    vscode.StatusBarAlignment.Left,
+    100000000
   );
+
   function runStatusBarFunction() {
     // TODO: fetch command from settings
-    const path = context
-      .asAbsolutePath("/")
-      .split("/")
-      .filter(p => !!p);
+
     if (vscode.workspace.name) {
-		statusBarItem.text = vscode.workspace.name;
-		statusBarItem.show();
+      statusBarItem.text = transformFolderName(vscode.workspace.name);
+      statusBarItem.show();
     } else {
-      statusBarItem.text = path[path.length - 1].toUpperCase();
+      const path = context
+        .asAbsolutePath("/")
+        .split("/")
+        .filter(p => !!p);
+
+      statusBarItem.text = transformFolderName(path[path.length - 1]);
       statusBarItem.show();
     }
-    // statusBarItem.hide();
   }
 
   let disposable = vscode.commands.registerCommand(
